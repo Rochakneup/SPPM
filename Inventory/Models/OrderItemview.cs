d20a62dcdcd2b8533eb165df5fad3b2a7f404117
Dangolabhaya99 <abhayadangol100@gmail.com>
2025-03-27 13:11:05 +0545

    Final

---
+++ Inventory/Models/OrderItemview.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Inventory.Models
+{
+    public class OrderViewModel
+    {
+        public List<OrderItemViewModel> OrderItems { get; set; }
+
+        // You can add additional fields here as needed
+    }
+
+    public class OrderItemViewModel
+    {
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
